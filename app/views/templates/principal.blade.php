c73feb1012c399b1aa3c1e5283be646ad8903280
Lucasamorimpassos <lucasamorimpassos1983@gmail.com>
2023-04-03 11:30:29 -0300

    início Template

--- app/views/templates/principal.blade.php
+++ app/views/templates/principal.blade.php
@@ -1,34 +1,54 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="pt">
-    <head>
-        <meta charset="UTF-8">
-        <link rel="shortcut icon" href="favicon.png">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        <title>Casa do Evangélico</title>
-
-        <style>
-            nav{
-                background-color: blue;
-            }
-        </style>
-    </head>
-    <body>
-        <nav>
-            <div class="nav-wrapper">
-                <a href="#" class="brand-logo">Logo</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">JavaScript</a></li>
-                </ul>
-            </div>
-        </nav>
-
-        @yield('conteudo')
-
-        <footer>
-
-        </footer>
-    </body>
+  <head>
+    <meta charset="UTF-8">
+    <title>Casa do Evangélico</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Roupas Masculinas</a></li>
+          <li><a href="#">Roupas Femininas</a></li>
+          <li><a href="#">Roupas Infantis</a></li>
+          <li><a href="#">Assessórios</a></li>
+          <li><a href="#">Decoração</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section>
+        <h1>Bem-vindo à Casa do Evangélico</h1>
+        <p>Encontre aqui as melhores opções de roupas e acessórios para a sua família!</p>
+      </section>
+      <section>
+        <h2>Roupas Masculinas</h2>
+        <p>Confira a nossa seleção de roupas masculinas para todas as ocasiões.</p>
+      </section>
+      <section>
+        <h2>Roupas Femininas</h2>
+        <p>Descubra as nossas opções de roupas femininas, desde vestidos elegantes até roupas casuais para o dia a dia.</p>
+      </section>
+      <section>
+        <h2>Roupas Infantis</h2>
+        <p>Deixe os seus filhos estilosos e confortáveis com as nossas roupas infantis.</p>
+      </section>
+      <section>
+        <h2>Assessórios</h2>
+        <p>Complemente o seu look com os nossos acessórios, desde chapéus até bolsas e carteiras.</p>
+      </section>
+      <section>
+        <h2>Decoração</h2>
+        <p>Deixe a sua casa ainda mais bonita e aconchegante com a nossa seleção de decoração.</p>
+      </section>
+      <section>
+        <h2>Entre em Contato</h2>
+        <p>Para mais informações sobre a nossa loja, entre em contato conosco pelo telefone ou email abaixo:</p>
+        <p>Telefone: (XX) XXXX-XXXX</p>
+        <p>Email: contato@casadoevangelico.com</p>
+      </section>
+    </main>
+  </body>
 </html>
