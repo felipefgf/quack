424047c6382441bc8d65408c191595c9b1f2449e
Lucasamorimpassos <lucasamorimpassos1983@gmail.com>
2023-04-04 10:32:19 -0300

    iniciei o template

--- app/views/templates/principal.blade.php
+++ app/views/templates/principal.blade.php
@@ -3,17 +3,53 @@
   <head>
     <meta charset="UTF-8">
     <title>Casa do Evangélico</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            
+
+    <style> 
+
+      section{height: 300px;
+
+      }
+
+      nav{background-color: blue;
+
+      }
+
+
+      nav li:hover{ background-color: black;
+
+      }
+
+      .z-depth-5{
+        -webkit-box-shadow: 0 24px 38px 3px rgba(0,0,200,0.14), 0 9px 46px 8px rgba(0,0,200,0.12), 0 11px 15px -7px rgba(0,0,200,0.2);
+        box-shadow: 0 24px 38px 3px rgba(0,0,200,0.14), 0 9px 46px 8px rgba(0,0,200,0.12), 0 11px 15px -7px rgba(0,0,200,0.2);
+
+
+
+      }
+
+
+
+  </style>
+
+
   </head>
   <body>
     <header>
-      <nav>
+      <nav class="z-depth-5">
+
         <ul>
           <li><a href="#">Início</a></li>
           <li><a href="#">Roupas Masculinas</a></li>
           <li><a href="#">Roupas Femininas</a></li>
           <li><a href="#">Roupas Infantis</a></li>
           <li><a href="#">Assessórios</a></li>
-          <li><a href="#">Decoração</a></li>
+          <li><a href="#decoracao">Decoração</a></li>
           <li><a href="#">Contato</a></li>
         </ul>
       </nav>
@@ -22,6 +58,7 @@
       <section>
         <h1>Bem-vindo à Casa do Evangélico</h1>
         <p>Encontre aqui as melhores opções de roupas e acessórios para a sua família!</p>
+        <a class="waves-effect waves-light btn">clique aqui</a>
       </section>
       <section>
         <h2>Roupas Masculinas</h2>
@@ -39,7 +76,7 @@
         <h2>Assessórios</h2>
         <p>Complemente o seu look com os nossos acessórios, desde chapéus até bolsas e carteiras.</p>
       </section>
-      <section>
+      <section id="decoracao">
         <h2>Decoração</h2>
         <p>Deixe a sua casa ainda mais bonita e aconchegante com a nossa seleção de decoração.</p>
       </section>
@@ -52,3 +89,4 @@
     </main>
   </body>
 </html>
+
