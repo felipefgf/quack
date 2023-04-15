e326c6dd09ea00756763cb00daefaea2d8d2b8a2
DenisFerreira90 <115656630+DenisFerreira90@users.noreply.github.com>
2023-04-15 20:36:06 -0300

    att

--- app/views/public/home.blade.php
+++ app/views/public/home.blade.php
@@ -8,4 +8,14 @@
   <div class="imagemprincipal">
       <img src='{{asset("imagens/CASADOEVANGELICONEWIMAGE.png")}}'>
   </div>
+  <div class='divprodutos'>
+    @for($i=0; $i < 8;$i++ )
+    <div class="divproduto">
+      <img src="https://img.ltwebstatic.com/images3_pi/2022/05/25/1653449022cd4bd2426a6887e96e5825bcd3f2ac19.webp" alt="">
+
+    </div>
+    @endfor
+  </div>
+  
+  
 @stop
--- app/views/templates/principal.blade.php
+++ app/views/templates/principal.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <link rel="stylesheet" href="{{asset('css/principal.css')}}">
+    @yield('css')
   </head>
   <body>
     <header>
