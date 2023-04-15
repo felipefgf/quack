86742e6a585335602028c748b8ef393604c3ee61
Felipe Fernandes <felipe_fernandes@ufrj.br>
2023-04-15 20:00:42 -0300

    Vez do Denis

--- app/views/public/home.blade.php
+++ app/views/public/home.blade.php
@@ -1,14 +1,10 @@
 @extends('templates.principal')
 
 @section('css')
-<link rel="stylesheet" href="{{asset('css/home.css')}}">
-
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
 @stop
 
 @section('conteudo')
-
-
-     
   <div class="imagemprincipal">
       <img src='{{asset("imagens/CASADOEVANGELICONEWIMAGE.png")}}'>
   </div>
